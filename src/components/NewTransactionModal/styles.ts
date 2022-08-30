30bbcd7350a9c8d0d865b7e15770576208b1fca3
andreenakashima <andreenakashima@gmail.com>
2022-08-30 11:32:57 -0300

    Criado radio button com acessibilidade

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import * as Dialog from '@radix-ui/react-dialog'
+import * as RadioGroup from '@radix-ui/react-radio-group'
 import styled from 'styled-components'
 
 export const Overlay = styled(Dialog.Overlay)`
@@ -68,7 +69,7 @@ export const CloseButton = styled(Dialog.Close)`
   color: ${(props) => props.theme['gray-500']};
 `
 
-export const TransactionType = styled.div`
+export const TransactionType = styled(RadioGroup.Root)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 1rem;
@@ -78,7 +79,9 @@ interface TransactionTypeButtonProps {
   variant: 'income' | 'outcome'
 }
 
-export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+export const TransactionTypeButton = styled(
+  RadioGroup.Item,
+)<TransactionTypeButtonProps>`
   background: ${(props) => props.theme['gray-600']};
   padding: 1rem;
   display: flex;
@@ -88,12 +91,29 @@ export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
   border-radius: 6px;
   border: 0;
   cursor: pointer;
-  color: ${(props) => props.theme['green-300']};
+  color: ${(props) => props.theme['gray-300']};
 
   svg {
     color: ${(props) =>
       props.variant === 'income'
-        ? props.theme['gray-300']
+        ? props.theme['green-300']
         : props.theme['red-300']};
+  }
+
+  &[data-state='unchecked']:hover {
+    background: ${(props) => props.theme['gray-600']};
+    transition: background-color 0.2s;
+  }
+
+  &[data-state='checked'] {
+    color: ${(props) => props.theme.white};
+    background: ${(props) =>
+      props.variant === 'income'
+        ? props.theme['green-500']
+        : props.theme['red-500']};
+
+    svg {
+      color: ${(props) => props.theme.white};
+    }
   }
 `
